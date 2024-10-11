aa294a61e362d0ec2803ef3e47353eb4f4522052
jajoh143 <jajoh143@gmail.com>
2024-10-11 13:26:28 -0500

    Added the base home page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,25 @@
+
+
+function Home(){
+
+    return (
+        <div className="bg-white m-8 p-8 rounded shadow-md">
+            <div className="container m-auto grid grid-cols-2">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-24 ml-auto mr-auto mb-1">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
+                    <h1 className="text-4xl pb-1">Jacob Johnson</h1>
+                    <h2 className="text-2xl overline">Software Developer Extraordinaire</h2>
+                </div>
+                <div>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
